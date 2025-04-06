2737a6c88b06eb3079e317930fd1c501c0c05da0
braintop <braintop7@gmail.com>
2025-04-06 14:36:19 +0300

    add into

---
+++ 01intro/app.js
@@ -0,0 +1,16 @@
+//output 
+console.log("hello world")
+//on java : int x = 5, float y = 5.5f; string name = "asaf"
+//on js : var x = 5; let y =5; const t = 5;
+
+const x = 5; 
+//x = 6;// TypeError :  Assignment to constant variable.
+console.log(x);//print x 
+let firstname = "Oren" // type is string 
+let age = 6;// type is integer number 
+console.log(firstname)// print first name 
+console.log(firstname + " is "+ age + " years old")
+
+let x1 = Math.random()//random number 0 - 1 
+console.log(x1)
+
