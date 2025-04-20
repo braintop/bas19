c9b38f19466d9e57109e4630a8f3d114d0e246cf
braintop <braintop7@gmail.com>
2025-04-20 15:30:58 +0300

    add

---
+++ 02arrays/05random_min_max.js
@@ -0,0 +1,19 @@
+//40 - 70 
+let num1 = 40 + Math.floor(Math.random() * (70 - 40))
+console.log(num1)
+
+let num2 = 40 + Math.floor(Math.random() * 30)
+console.log(num2)
+
+let min = 100
+let max = 200
+let number = min + Math.floor(Math.random() * (max - min))
+
+let min1 = Math.random() * 1000
+let max1 = Math.random() * 1000
+if (min1 > max1) {
+    temp = max1
+    max1 = min1
+    min1 = temp
+}
+let number2 = min1 + Math.floor(Math.random() * (max - min))
