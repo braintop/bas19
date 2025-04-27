4eb284d7dea2f387d15f50cf7972958b34c88ad4
braintop <braintop7@gmail.com>
2025-04-27 12:14:39 +0300

    add

--- 03functions/06min.js
+++ 03functions/06min.js
@@ -3,5 +3,13 @@
 // numbers = [10, 5, 18, 3, 22]
 
 function minInArr(numbers){
-
+    let min = numbers[0]
+    for(let i=0;i<numbers.length;i++){
+        if(min<numbers[i]) 
+            min = numbers[i]
+    }
+    return min;
 }
+let numbers = [10, 5, 18, 3, 22]
+let m = minInArr(numbers)
+console.log(m) 
