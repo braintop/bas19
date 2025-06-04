fa8ced676558e0b90d48b13b643f462e2667dd54
braintop <braintop7@gmail.com>
2025-06-04 11:48:39 +0300

    add

--- 13-express/index.js
+++ 13-express/index.js
@@ -1,5 +1,14 @@
 let express = require("express")
 let app = express()
 // get , post , delete , put , patch  
+app.get("/", (req, res) => {
+    res.json({ message: "first server" })
+})
+app.get("/books", (req, res) => {
+    res.json({ message: "bible" })
+})
+app.get("/books/harry", (req, res) => {
+    res.json({ title: "harry potter" })
+})
 
 app.listen(3000)
