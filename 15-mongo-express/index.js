1055f98760821ab21339ed5ed0c8eecc8da01cdd
braintop <braintop7@gmail.com>
2025-06-08 10:21:31 +0300

    add async connected

--- 15-mongo-express/index.js
+++ 15-mongo-express/index.js
@@ -5,7 +5,17 @@ let app = express();
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 const uri = "mongodb+srv://david:Aa123456@cluster0.gqvy99x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(uri, clientOptions);
+async function connectMongo() {
+    try {
+        await mongoose.connect(uri, clientOptions);
+        console.log("connected successfully to mongodb")
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+connectMongo()
 app.use(express.json())
 app.post('/api/v1/products', async function (req, res) {
     let body = req.body; // {title:"ball", description:"red","price":50}
