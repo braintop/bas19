d997381c99bbb88849d13ec1ba058222fcbbd380
braintop <braintop7@gmail.com>
2025-06-09 15:11:43 +0300

    add

--- 18-todo-user-refactoring/index.js
+++ 18-todo-user-refactoring/index.js
@@ -1,7 +1,25 @@
+//npm install cors
+//const cors=require("cors");
+// const corsOptions ={
+//    origin:'*', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
+
+// app.use(cors(corsOptions)) // Use this after the variable declaration
+
 let express = require('express');
 let mongoose = require('mongoose');
+const cors = require("cors");
 let app = express();
 app.use(express.json())
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 const uri = "mongodb+srv://david:Aa123456@cluster0.gqvy99x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 var todoRouter = require("./api/todos/TodoRoute")
--- 19-js-example/main.js
+++ 19-js-example/main.js
@@ -1,6 +1,80 @@
 
-async function login() {
-    alert("login")
+async function login(email, password, apiUrl) {
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email: email, password: password }),
+        });
+
+        // if (!response.ok) {
+        //     throw new Error(`HTTP error! status: ${response.status}`);
+        // }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error during login:', error);
+        throw error; // Re-throw the error to be handled by the caller
+    }
 }
 
-login()
+async function getTodos(apiUrl) {
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        // if (!response.ok) {
+        //     throw new Error(`HTTP error! status: ${response.status}`);
+        // }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error during login:', error);
+        throw error; // Re-throw the error to be handled by the caller
+    }
+}
+
+async function f() {
+    try {
+        let e = document.getElementById("email").value
+        let p = document.getElementById("password").value
+
+        let data = await login(e, p, "http://127.0.0.1:3000/api/v1/users/login")
+        console.log(data);
+        if (data.login) {
+            document.getElementById("result").innerText = data.message
+            document.getElementById("result").style.color = "green"
+        }
+        else {
+            document.getElementById("result").innerText = data.message
+            document.getElementById("result").style.color = "red"
+        }
+    }
+    catch (err) {
+        console.log(err)
+        document.getElementById("result").innerText = data.message
+        document.getElementById("result").style.color = "red"
+
+    }
+}
+
+async function getAllTodos() {
+    try {
+        let data = await getTodos("http://127.0.0.1:3000/api/v1/todos")
+        console.log("try to fetch")
+        console.log(data);
+    }
+    catch (err) {
+        console.log(err)
+
+    }
+
+}
